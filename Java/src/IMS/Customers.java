f7c27a1f2b454faff7fc61d83c5f238a4dcf87cf
Onur <107554158+On-ur@users.noreply.github.com>
2022-07-14 16:31:58 +0100

    Add files via upload

--- Java/src/IMS/Customers.java
+++ Java/src/IMS/Customers.java
@@ -6,5 +6,13 @@ public class Customers extends Items & Orders{
      private String first_name;
      private String last_name;
 
+     public class Customers(){
+
+          public class Customers(int customer_id, String first_name, String last_name){
+               this.customer_id = customer_id;
+               this.first_name = first_name;
+               this.last_name = last_name;
+          }
+     }
 
 }
