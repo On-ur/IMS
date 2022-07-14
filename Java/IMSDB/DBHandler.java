2f27a5836c93ded1d33254efe69bb1257b62dbfd
Onur <107554158+On-ur@users.noreply.github.com>
2022-07-14 02:31:05 +0100

    Add files via upload

--- Java/IMSDB/DBHandler.java
+++ Java/IMSDB/DBHandler.java
@@ -1,2 +1,44 @@
 public class DBHandler {
+
+    import java.util.*;
+
+    /**
+     * Insert an item into the database
+     * @param item
+     * @return
+     */
+
+    public boolean insertItem(Item item){
+
+        boolean result = false;
+
+        try{
+
+            //SQL
+            String sql = "Insert into Item VALUES(?, ?, ?, ?)";
+            Connection connection = DBConnection.getConnection();
+
+            PrepareStatement stmt = connection.prepareStatement(sql);
+
+            stmt.setInt(1, item.getItem_id());
+            stmt.setString(2, item.getItem_name());
+            stmt.setInt(3, item.getQuantity());
+            stmt.setDouble(4, item.getPrice());
+
+            //Execute the command
+            int inserted = stmt.executeUpdate();
+
+            result = inserted >= 1;
+
+            DBConnection.disconnect(connection);
+            
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return result;
+
+    }
 }
--- Java/src/IMS/Orders.java
+++ Java/src/IMS/Orders.java
@@ -1,2 +1,4 @@
 public class Orders {
+
+    private int order_id;
 }
