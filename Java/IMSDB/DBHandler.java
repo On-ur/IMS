bc64c47b839a5be6f6a24492679649e010abf958
Onur <107554158+On-ur@users.noreply.github.com>
2022-07-14 04:49:50 +0100

    Add files via upload

--- Java/IMSDB/DBHandler.java
+++ Java/IMSDB/DBHandler.java
@@ -41,4 +41,102 @@ public class DBHandler {
         return result;
 
     }
+
+    /**
+     * Delete an item with id
+     *
+     * @param id
+     */
+    public void deleteItem(int id){
+        boolean result = false;
+
+        try{
+            String sql = "Delete From Items Where item_id = ?";
+            connection conn = DBconnection.getConnection();
+
+            PrepareStatement stmt = conn.prep
+                    stmt.setInt(1, id);
+
+            int delete = stmt.executeUpdate();
+
+            result = delete >= 1;
+
+            DBConnection.disconnect(conn);
+
+
+        }catch(Exception e){
+            ee.printStackTrace();
+        }
+
+        return result;
+
+
+    }
+    /**
+     * Edit an item with id
+     *
+     * @param id
+     */
+    public void updateItem(Item item){
+        boolean result = false;
+
+        try{
+
+            String sql = "Update Item, Set Quantity =?, Price=? Where ItemId=?";
+            Connection conn = DBconnection.getConnection();
+
+            PreparedStatement stmt = conn.prepareStatement();
+            stmt.setInt(1, item.getQuantity());
+            stmt.setDouble(2, item.getPrice());
+            stmt.setInt(3, item.getitemId());
+
+            int updated = stmt.executeUpdate();
+
+            result = updated >=1:
+
+
+            DBconnection.disconnect(conn);
+
+        }catch(Exception e){
+            ee.printStackTrace();
+        }
+
+        return result;
+
+
+    }
+
+    /**
+     * Select an item with id
+     *
+     * @param id
+     * @return item
+     */
+    public Item getItem(int id){
+        Item item = null;
+
+        try {
+
+            String sql = "Select From Items, Where Itemid=?";
+            Connection conn = DBconnection.getConnection();
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()){
+
+                item = new Item(rs.getInt(1(, rs.getString(2), rs.getInt(3), rs.getDouble(4));
+
+            }
+
+            DBConnection.disconnect(conn);
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+    }
+        return item;
 }
