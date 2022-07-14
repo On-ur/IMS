de9815685f041bbcf931b57d68371aeb17593d97
Onur <107554158+On-ur@users.noreply.github.com>
2022-07-14 01:16:23 +0100

    Add files via upload

--- Java/IMSDB/DBConnection.java
+++ Java/IMSDB/DBConnection.java
@@ -1,5 +1,24 @@
 public class DBConnection {
 
+    import java.sql.*;
+
     // DB parameters
-    private static final String URL = "jdbc:mysql"
+    private static final String URL = "jdbc:mysql://rds-free.crktn2ycvrwh.eu-west-2.rds.amazonaws.com:3306" ;
+    private static final String USER = "root" ;
+    private static final String PASSWORD = "ochroot256" ;
+    private static final String DBNAME = "IMSDB";
+    private static final String PARAMETERS = "?verifyServerCertificate=false&useSSL=true";
+
+    // Connection
+    Public static Connection getConnection(){
+       Connection conn - null;
+
+       try{
+
+       }catch(Exception e){
+           e.print
+       }
+
+       return conn;
+    }
 }
